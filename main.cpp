a55f676d225d058b85e017d5d077abe9effe1087
putrifaradilah <putrifaradilah491@gmail.com>
2025-10-09 18:08:26 +0700

    Lihat-daftar-reminder

--- main.cpp
+++ main.cpp
@@ -166,6 +166,7 @@ int main() {
         }
     } while (dontClosetheProgram);
 
+    int main(){
     void lihatreminder(vector<string>judul, vector<string>deadline, vector<string>kategori, vector<string>status){
     
     if (judul.size() == 0) {
@@ -183,6 +184,6 @@ int main() {
     }
     cout << "Total: " << judul.size() << " reminder\n";
 }
-    
+}
     return 0;
 }
